9ded4b2ae834d1243d35c02dab59ec0e7b9f4551
Student <jredota.8899@umak.edu.ph>
2026-09-24 20:19:53 +0800

    Midterm Exam

---
+++ main.py
@@ -0,0 +1,13 @@
+quantity = 0
+price_per_unit = 0
+
+choice = input("select an option (1-4): ")
+if choice == "1":
+    input("Enter the Item Name: ")
+    int(input("Enter Quantity sold: "))
+    float(input("Enter Price per Unit: "))
+
+elif choice == "4":
+    print("Thank you for using the Sales Record Management System.")
+    exit()
+
